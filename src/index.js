68deb86580408c920878b54c8cafd6a63698acb3
John Pawlak <johnpawlak@gmail.com>
2018-09-11 15:13:56 -0400

    CardList and full robots object added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import Card from './Card';
+import CardList from './CardList';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 import { robots } from './robots';
@@ -10,8 +10,6 @@ import { robots } from './robots';
 ReactDOM.render(
     <div className="tc">
         <h1 className="f1">ROBOFRIENDS APP</h1>
-        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+        <CardList robots={robots}/>
     </div>, document.getElementById('root'));
 registerServiceWorker();
