86d166fbb362de8874429f10787cdecea4d1bf04
John Pawlak <johnpawlak@gmail.com>
2018-09-11 13:59:07 -0400

    missed Cardcomponent added

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import robots from './robots';
+
+const Card = ({ name, email, id }) => {
+    
+    return (
+        // let url = 'https://robohash.org/' + {props.id} +'?size=200x200';
+        <div className="bg-light-green dib br3 ma2 grow bw2 shadow-5">
+            {/* <h1 className = 'tc f1'>ROBOFRIENDS</h1> */}
+            <img src= {`https://robohash.org/${id}?size=200x200`} alt="robots" />
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    )
+}
+export default Card;
